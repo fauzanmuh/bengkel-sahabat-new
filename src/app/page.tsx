4cd7260f6c498c16441344929175f359c4920ec3
fauzanmuh <f4uz4n.oioi@gmail.com>
2025-06-09 12:20:30 +0700

    fix: setup css and font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
-import Image from "next/image";
-import "./css/style.css";
+import "@/css/style.css";
 
 export default function Home() {
   return (
     <>
       <div className="flex items-center justify-evenly gap-2 text-2xl text-green-400">
-        <h1>Home</h1>
+        <h1>
+          Home Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+          Temporibus maxime dicta recusandae ullam blanditiis explicabo minus
+          quia error saepe voluptatem vel beatae provident, amet iure harum
+          culpa porro reiciendis mollitia.
+        </h1>
       </div>
     </>
   );
